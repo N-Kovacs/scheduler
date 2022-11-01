03456740e21905d6051e0eb132e3511f77cfdfe4
N.Kovacs <nicholasderekkovacs@gmail.com>
2022-11-01 17:39:18 -0400

    daylistitem testing + spot conditionals

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -3,13 +3,25 @@ import classNames from "classnames";
 
 import "components/DayListItem.scss";
 
+const formatSpots = function(number){
+  let output = ""
+  if (number > 1){
+    output = <h3 className="text--light">{number} spots remaining</h3>
+  } else if (number === 1){
+    output = <h3 className="text--light">1 spot remaining</h3>
+  } else {
+    output = <h3 className="text--light">no spots remaining</h3>
+  }
+  return output
+}
+
 export default function DayListItem(props) {
   let dayClass = classNames("day-list__item", {"day-list__item--selected": props.selected}, {"day-list__item--full": props.spots===0} )
 
   return (
     <li className={dayClass} onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+      {formatSpots(props.spots)}
     </li>
   );
 }
