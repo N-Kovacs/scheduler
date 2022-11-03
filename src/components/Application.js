e544df705f072643142733bfbd81f66f48f49215
N.Kovacs <nicholasderekkovacs@gmail.com>
2022-11-03 17:30:11 -0400

    working days and appointments view

--- src/components/Application.js
+++ src/components/Application.js
@@ -6,66 +6,92 @@ import DayList from "./DayList";
 import "components/Application.scss";
 import "components/Appointment";
 import Appointment from "components/Appointment";
+import { getAppointmentsForDay } from "helpers/selectors";
 
-
-const appointments = {
-  1: {
-    id: 1,
-    time: "12pm",
-  },
-  2: {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer: {
-        id: 3,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      },
-    },
-  },
-  3: {
-    id: 3,
-    time: "2pm",
-  },
-  4: {
-    id: 4,
-    time: "3pm",
-    interview: {
-      student: "Archie Andrews",
-      interviewer: {
-        id: 4,
-        name: "Cohana Roy",
-        avatar: "https://i.imgur.com/FK8V841.jpg",
-      },
-    },
-  },
-  5: {
-    id: 5,
-    time: "4pm",
-  },
-};
+// const appointments = {
+//   1: {
+//     id: 1,
+//     time: "12pm",
+//   },
+//   2: {
+//     id: 2,
+//     time: "1pm",
+//     interview: {
+//       student: "Lydia Miller-Jones",
+//       interviewer: {
+//         id: 3,
+//         name: "Sylvia Palmer",
+//         avatar: "https://i.imgur.com/LpaY82x.png",
+//       },
+//     },
+//   },
+//   3: {
+//     id: 3,
+//     time: "2pm",
+//   },
+//   4: {
+//     id: 4,
+//     time: "3pm",
+//     interview: {
+//       student: "Archie Andrews",
+//       interviewer: {
+//         id: 4,
+//         name: "Cohana Roy",
+//         avatar: "https://i.imgur.com/FK8V841.jpg",
+//       },
+//     },
+//   },
+//   5: {
+//     id: 5,
+//     time: "4pm",
+//   },
+// };
 
 export default function Application(props) {
-  const Mapappointments = Object.values(appointments).map((appointment) => {
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    // you may put the line below, but will have to remove/comment hardcoded appointments variable
+    appointments: {},
+  });
+
+  let dailyAppointments = [];
+
+  const setDay = (day) => setState({ ...state, day });
+
+  // const setDays = (days) => {
+  //   setState(prev => ({ ...prev, days }));
+  // }
+
+
+  let getAppointmentsobj = {
+    days: state.days,
+    appointments: state.appointments
+  };
+
+  dailyAppointments = getAppointmentsForDay(
+    getAppointmentsobj,
+    state.day
+  );
+  let Mapappointments = dailyAppointments.map((appointment) => {
     return <Appointment key={appointment.id} {...appointment} />;
   });
 
-  const [day, setDay] = useState("Monday");
-  const [interviewer, setInterviewer] = useState("");
-  const [days, setDays] = useState([]);
-
   useEffect(() => {
-    const testURL = `http://localhost:8001/api/days`;
-    axios.get(testURL).then(response => {
-      console.log(response.data)
-      setDays(response.data)
-    });
-  }, [])
+    Promise.all([axios.get("/api/days"), axios.get("/api/appointments")]).then(
+      (all) => {
+        console.log(all[0].data);
 
-  
+        console.log(getAppointmentsobj);
 
+        setState((prev) => ({
+          ...prev,
+          days: all[0].data,
+          appointments: all[1].data,
+        }));
+      }
+    );
+  }, []);
 
   return (
     <main className="layout">
@@ -77,7 +103,7 @@ export default function Application(props) {
         />
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
-          <DayList days={days} day={day} onChange={setDay} />
+          <DayList days={state.days} day={state.day} onChange={setDay} />
         </nav>
         <img
           className="sidebar__lhl sidebar--centered"
@@ -85,8 +111,10 @@ export default function Application(props) {
           alt="Lighthouse Labs"
         />
       </section>
-      <section className="schedule">{Mapappointments}<Appointment key="last" time="5pm" />
-</section>
+      <section className="schedule">
+        {Mapappointments}
+        <Appointment key="last" time="5pm" />
+      </section>
     </main>
   );
 }
